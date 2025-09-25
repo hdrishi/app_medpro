412aa142461a59310cbec172925c6f6f602516d3
Hrishikesh Das <hdrishi@gmail.com>
2025-09-25 16:10:17 +0530

    base commit

--- medpro/src/App.tsx
+++ medpro/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Master from "./features/dashboard/components/Master";
 
-function App() {
+const App = () => {
   return (
     <React.Fragment>
       <Master />
     </React.Fragment>
   );
-}
+};
 
 export default App;
---
+++ medpro/src/features/dashboard/components/BrandLogo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BrandLogo = () => {
+  return (
+    <img
+      src="../medpro.jpeg"
+      alt="Logo"
+      style={{ height: "60px", width: "200px" }}
+    />
+  );
+};
+
+export default BrandLogo;
